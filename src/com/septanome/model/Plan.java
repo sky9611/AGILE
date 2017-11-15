e21d8086a3b2d181675c8a30de4344472ab0e0fe
Zhewei Tu <zhewei.tu@insa-lyon.fr>
2017-11-15 10:56:45 +0100

    plan

---
+++ src/com/septanome/model/Plan.java
@@ -0,0 +1,12 @@
+package com.septanome.model;
+
+import java.util.*;
+
+public class Plan {
+	private List<Point> pointsLivraison;
+	private List<Route> routes;
+	
+	public Plan(file filename) {
+		//A completer..
+	}
+}

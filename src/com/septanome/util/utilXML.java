87820c19cdbd48e6eb510e9ff35f3f365e2f8740
echire <matthieuricheinsa@gmail.com>
2017-11-15 11:36:43 +0100

    edit utilXML

--- src/com/septanome/util/utilXML.java
+++ src/com/septanome/util/utilXML.java
@@ -12,6 +12,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import com.septanome.model.Point;
+import com.septanome.model.Troncon;
 
 
 public class utilXML {
@@ -33,7 +34,7 @@ public class utilXML {
 				Node nNode = nList.item(temp);
 				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 					Element eElement = (Element) nNode;
-					int id = Integer.parseInt(eElement.getAttribute("id"));
+					Long id = Long.parseLong(eElement.getAttribute("id"));
 					int x = Integer.parseInt(eElement.getAttribute("x"));
 					int y = Integer.parseInt(eElement.getAttribute("y"));
 					point = new Point(id, x, y);
@@ -41,6 +42,40 @@ public class utilXML {
 				} 
 			}
 			return points;
+		    } catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		    } 
+	}
+	
+	
+	public Vector<Troncon> loadTroncon(String folder) {
+		Vector<Troncon> troncons = new Vector<Troncon>(); 
+		Troncon troncon;
+		
+		try {
+			File fXmlFile = new File(folder);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+
+			doc.getDocumentElement().normalize();
+			doc.getDocumentElement().getNodeName();
+			NodeList nList = doc.getElementsByTagName("troncon");			
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				Node nNode = nList.item(temp);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element eElement = (Element) nNode;
+					Long dest = Long.parseLong(eElement.getAttribute("destination"));
+					double length = Double.parseDouble(eElement.getAttribute("longueur"));
+					String street = eElement.getAttribute("nomRue");
+					Long origine = Long.parseLong(eElement.getAttribute("origine"));
+					troncon = new Troncon(dest, length, street, origine);					
+					troncons.add(troncon);	
+					
+				} 
+			}
+			return troncons;
 		    } catch (Exception e) {
 			e.printStackTrace();
 			return null;

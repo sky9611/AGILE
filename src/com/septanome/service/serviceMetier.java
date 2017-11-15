d442800ab563c205f9a24c417bac12382fd0025c
sky9611 <collinbeyond@gmail.com>
2017-11-15 11:53:21 +0100

    serviceMetier created

---
+++ src/com/septanome/service/serviceMetier.java
@@ -0,0 +1,14 @@
+package com.septanome.service;
+
+import java.io.File;
+
+public class serviceMetier {
+	
+	/**
+	* initialise le plan
+	* @nomFicher: nom du ficher xml ид lire  
+	*/
+	public void initPlan(String nomFicher) {
+		
+	}
+}

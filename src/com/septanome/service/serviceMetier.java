384e5fbefb9aec328b9ec08f4044d91a3599fc13
sky9611 <collinbeyond@gmail.com>
2017-11-16 00:11:25 +0100

    TODO in ServiceMetier

--- src/com/septanome/service/serviceMetier.java
+++ src/com/septanome/service/serviceMetier.java
@@ -2,18 +2,48 @@ package com.septanome.service;
 
 import java.io.File;
 
-import com.septanome.model.Plan;
+import com.septanome.model.*;
 
 public class serviceMetier {
 	private Plan plan;
+	private Plan planLivraison;
+	private Commande commande;
+	private Tournee tournee;
 	
 	/**
-	* initialise le plan
-	* @nomFicher: nom du ficher xml ид lire  
+	* Initialiser le plan total a partir d'un ficher XML
+	* @nomFicherDePlan: nom du ficher xml a lire  
 	*/
-	public void initPlan(String nomFicher) {
+	public void initPlan(String nomFicherDePlan) {
+		//TODO
+	}
+	
+	/**
+	 *Initialiser la commande a partir d'un ficher XML
+	 */
+	public void initCommande(String nomFicherDeCommande) {
 		
 	}
 	
+	/**
+	 *Initialiser le plan avec que les points de livrson et les routes le plus court entre eux calcule par dijkstra 
+	 */
+	public void initPlanLivraison() {
+		//TODO
+	}
 	
+	/**
+	 *Chercher dans le Plan total le longeur de chemin plus court de livraison origine vers destination
+	 */ 
+	public int calcLePlusCourtChemin(long destinationID, long origineID) {
+		//TODO
+		return 0;
+	}
+	
+	/**
+	 *Trouver le tournee final en utlisant le plan de livraison genere
+	 */
+	public void obtenirLeTournee() {
+		//TODO
+	}
 }

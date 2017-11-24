a0c75f4bcf89f80f9ebd3a818d9f37a1ac082ca1
sky9611 <collinbeyond@gmail.com>
2017-11-24 21:21:27 +0100

    TSPTW start'

--- src/main/java/com/septanome/util/MyTSP.java
+++ src/main/java/com/septanome/util/MyTSP.java
@@ -5,18 +5,6 @@ import java.util.Iterator;
 
 import tsp.TemplateTSP;
 
-public class MyTSP extends TemplateTSP{
-
-	@Override
-	protected int bound(Integer sommetCourant, ArrayList<Integer> nonVus, int[][] cout, int[] duree) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	protected Iterator<Integer> iterator(Integer sommetCrt, ArrayList<Integer> nonVus, int[][] cout, int[] duree) {
-		// TODO Auto-generated method stub
-		return nonVus.iterator();
-	}
-
+public class TSPTW {
+	
 }

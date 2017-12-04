94318d9023abec6c359db19a87e8c7f2893400cf
lulucasono <lulucasono@gmail.com>
2017-12-04 11:29:26 +0100

    Exception handling now exceptions are caught instead of thrown

--- src/test/java/serviceTests/ServiceMetierTest.java
+++ src/test/java/serviceTests/ServiceMetierTest.java
@@ -1,18 +1,31 @@
 package serviceTests;
 
+import com.septanome.exception.BadLinkException;
+import com.septanome.exception.ConstructorException;
+import com.septanome.model.Chemin;
+import com.septanome.model.Tournee;
 import org.junit.Test;
 import com.septanome.service.ServiceMetier;
 
 import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class ServiceMetierTest {
-    @Test public void test() throws IOException, ClassNotFoundException {
+    @Test public void testGetTournee() throws ConstructorException {
         ServiceMetier sm = new ServiceMetier();
-        sm.initPlan("./src/test/resources/fichiersXML/testPlan.xml");
-        sm.initCommande("./src/test/resources/fichiersXML/testLivraisons.xml");
+        sm.initPlan("./src/test/resources/fichiersXML/planLyonGrand.xml");
+        sm.initCommande("./src/test/resources/fichiersXML/DLGrand10.xml");
         sm.initPlanLivraison();
-        sm.calculerTournee(false);
-
-        System.out.println(sm.getTournee());
+        try {
+            sm.calculerTournee(false);
+        }catch (IOException ioe){
+            fail("IOException");
+        }catch(ClassNotFoundException cne){
+            fail("ClassNotFoundException");
+        }
+        sm.getTournee();
     }
 }

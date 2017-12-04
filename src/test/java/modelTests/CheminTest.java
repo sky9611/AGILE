21dabf6bb136f2a33b1dff0f58bb02068f57271c
lulucasono <lulucasono@gmail.com>
2017-12-04 10:47:41 +0100

    constructorTest now fails when BadLinkException or EmptyListException are thrown.

--- src/test/java/modelTests/CheminTest.java
+++ src/test/java/modelTests/CheminTest.java
@@ -1,5 +1,7 @@
 package modelTests;
 
+import com.septanome.exception.BadLinkException;
+import com.septanome.exception.EmptyListException;
 import com.septanome.model.Chemin;
 import com.septanome.model.Troncon;
 import org.junit.Test;
@@ -8,18 +10,25 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 
 public class CheminTest {
-    @Test public void testCheminConstructor(){
+    @Test public void testCheminConstructor (){
         Troncon t1 = new Troncon(2,0.5,"Rue Alsace",1);
         Troncon t2 = new Troncon(3,0.3,"Rue Lorraine",2);
         List<Troncon> troncons = Arrays.asList(t1,t2);
-        Chemin cheminUnderTest = new Chemin(t2.getDestinationID(),t1.getOrigineID(),troncons);
-        assertTrue("Troncon list should match",cheminUnderTest.getTroncons().equals(troncons));
-        assertTrue("Total length should match",cheminUnderTest.getLongeur()==(t1.getLongeur()+t2.getLongeur()));
-        assertTrue("OriginID should match first Troncon origin ID",cheminUnderTest.getOriginePointID()==t1.getOrigineID());
-        assertTrue("DestinationID should match last Troncon destination ID",cheminUnderTest.getDestinationPointID()==t2.getDestinationID());
+        try {
+            Chemin cheminUnderTest = new Chemin(t2.getDestinationID(), t1.getOrigineID(), troncons);
+            assertTrue("Troncon list should match", cheminUnderTest.getTroncons().equals(troncons));
+            assertTrue("Total length should match", cheminUnderTest.getLongeur() == (t1.getLongeur() + t2.getLongeur()));
+            assertTrue("OriginID should match first Troncon origin ID", cheminUnderTest.getOriginePointID() == t1.getOrigineID());
+            assertTrue("DestinationID should match last Troncon destination ID", cheminUnderTest.getDestinationPointID() == t2.getDestinationID());
+        }catch (BadLinkException ble){
+            fail("BadLink fail not expected");
+        }catch (EmptyListException ele){
+            fail("Empty list exception not expected");
+        }
     }
 
     /**

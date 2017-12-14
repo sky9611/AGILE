018b784f52d01db0d4afa8e8e68d0f5296665a1f
osubou <oussama.boukich@insa-lyon.fr>
2017-12-14 20:18:33 +0100

    Chemin documentation

--- src/main/java/com/septanome/model/PlanLivraison.java
+++ src/main/java/com/septanome/model/PlanLivraison.java
@@ -6,6 +6,11 @@ public class PlanLivraison {
     private HashMap<Long,Livraison> livraisonsMap;
     private HashMap<Long,HashMap<Long,Chemin>> cheminsMap;
 
+    /**
+     * Creer un plan de livraison:
+     * @param livraisonsMap
+     * @param cheminsMap
+     */
     public PlanLivraison(HashMap<Long,Livraison> livraisonsMap, HashMap<Long,HashMap<Long,Chemin>> cheminsMap) {
         this.livraisonsMap = livraisonsMap;
         this.cheminsMap = cheminsMap;
@@ -15,18 +20,35 @@ public class PlanLivraison {
         // TODO Auto-generated constructor stub
     }
 
+    /**
+     * Obtenir les livraisons
+     * @return HashMap<Long,Livraison> livraisonsMap
+     */
     public HashMap<Long,Livraison> getLivraisonsMap(){
         return livraisonsMap;
     }
+
+    /**
+     * Remplacer les livraisons
+     * @param HashMap<Long,Livraison> nouvelleLivraisonsMap
+     */
 
     public void setLivraisonsMap(HashMap<Long,Livraison> livraisonsMap) {
         this.livraisonsMap = livraisonsMap;
     }
 
+    /**
+     * Obtenir les chemin,
+     * @return HashMap<Long,HashMap<Long,Chemin>>  cheminsMap
+     */
     public HashMap<Long,HashMap<Long,Chemin>> getCheminsMap(){
         return cheminsMap;
     }
 
+    /**
+     * Remplacer les chemin
+     * @param HashMap<Long,HashMap<Long,Chemin>> nouvelleCheminsMap.
+     */
     public void setCheminsMap(HashMap<Long,HashMap<Long,Chemin>> cheminsMap) {
         this.cheminsMap = cheminsMap;
     }
